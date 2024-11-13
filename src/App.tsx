517319b8eb292a13c9fcc8bfcdcb0fc4bc12f5c0
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2024-11-12 19:30:35 -0800

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Welcome from "./pages/Welcome";
-import Login from "./pages/Login"
-import CreateAccount from "./pages/CreateAccount"
-import Home from "./pages/Home"
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
 import Bio from "./pages/Bio";
 import ProfilePhoto from "./pages/ProfilePhoto";
@@ -15,10 +15,13 @@ import EventConfirmation from "./pages/EventConfirmation";
 import EventAttendees from "./pages/EventAttendees";
 import UserProfile from "./pages/UserProfile";
 import UserMessage from "./pages/UserMessage";
+import Header from "@/components/layout/Header";
 import NavigationBar from "@/components/layout/NavigationBar";
 
-function App() {
-  const hideNavbarPaths = [
+const AppContent = () => {
+  const location = useLocation();
+
+  const noLayoutComponentsPaths = [
     "/",
     "/login",
     "/create-account",
@@ -28,37 +31,46 @@ function App() {
   ];
 
   return (
-    <div style={{width: '500px', height: '100vh', borderColor:'black', borderWidth: '2px'}}>
-      <Router>
-        <Routes>
-          {/* auth & account reaction */}
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/create-account/profile-photo" element={<ProfilePhoto />} />
-          <Route path="/create-account/bio" element={<Bio />} />
-          <Route path="/create-account/interests" element={<Interests />} />
+    <div style={{ width: '500px', height: '100vh', borderColor: 'black', borderWidth: '2px' }}>
+      {!noLayoutComponentsPaths.includes(location.pathname) && <Header />}
 
-          {/* Navbar/ main pages */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/edit" element={<EditProfile />} />
-          <Route path="/search" element={<Search />} />
+      <Routes>
+        {/* auth & account creation routes */}
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/create-account/profile-photo" element={<ProfilePhoto />} />
+        <Route path="/create-account/bio" element={<Bio />} />
+        <Route path="/create-account/interests" element={<Interests />} />
 
-          {/* event routes */}
-          <Route path="/event/:eventId/confirmation" element={<EventConfirmation />} />
-          <Route path="/event/:eventId/attendees" element={<EventAttendees />} />
+        {/* Navbar/ main pages */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/edit" element={<EditProfile />} />
+        <Route path="/search" element={<Search />} />
 
-          {/* user routes */}
-          <Route path="/user/:userId/profile" element={<UserProfile />} />
-          <Route path="/user/:userId/message" element={<UserMessage />} />
-        </Routes>
+        {/* event routes */}
+        <Route path="/event/:eventId/confirmation" element={<EventConfirmation />} />
+        <Route path="/event/:eventId/attendees" element={<EventAttendees />} />
 
-        {!hideNavbarPaths.includes(window.location.pathname) && <NavigationBar />}
-      </Router>
+        {/* user routes */}
+        <Route path="/user/:userId/profile" element={<UserProfile />} />
+        <Route path="/user/:userId/message" element={<UserMessage />} />
+      </Routes>
+
+      {!noLayoutComponentsPaths.includes(location.pathname) && <NavigationBar />}
     </div>
   );
-}
+};
+
+// done separate like this because we need access to Router for location at time of useLocation() call
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
 
 export default App;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,15 @@
-const Header = () => { }
+import React from 'react';
+import { ReactComponent as Logo } from "@/assets/images/icon.svg";
 
-export default Header
+const Header = () => {
+  return (
+    <div style={{
+      position: 'fixed', top: 0, left: 0, width: '500px', height: '65px',
+      backgroundColor: '#FFF5D0', display: 'flex', alignItems: 'center',
+    }}>
+      <Logo width={65} height={65} />
+    </div>
+  );
+};
+
+export default Header;
