da883b68df375619b960eea2b902d282205bedc9
s-iloo <igloo405@gmail.com>
2024-11-14 14:06:59 -0800

    home page events are now clickable and renders toast, added navigation for hive logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 import { ReactComponent as Logo } from "@/assets/images/icon.svg";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div style={{
-      position: 'sticky', top: 0, left: 0, width: '500px', height: '65px',
-      backgroundColor: '#FFF5D0', display: 'flex', alignItems: 'center', zIndex:'4'
-    }}>
-      <Logo width={65} height={65} />
+    <div
+      style={{
+        position: "sticky",
+        top: 0,
+        left: 0,
+        width: "500px",
+        height: "65px",
+        backgroundColor: "#FFF5D0",
+        display: "flex",
+        alignItems: "center",
+        zIndex: "4",
+      }}
+    >
+      <Logo
+        onClick={() => {
+          navigate("/home");
+        }}
+        width={65}
+        height={65}
+        style={{ cursor: "pointer" }}
+      />
     </div>
   );
 };
